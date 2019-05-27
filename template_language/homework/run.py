22331f8f9a70a7a78f8c21d622ac6e5676de170e
Andrey Pesotskiy <andrepesotskiy@gmail.com>
2019-05-27 21:15:45 +0300

    feat: hw7 done

--- template_language/homework/run.py
+++ template_language/homework/run.py
@@ -4,26 +4,39 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 with open('movies.json') as f:
     MOVIES = json.load(f)
 
 
 @app.route('/')
 def home_page():
-    return render_template('home.html', title='Home')
+    return render_template('home.html', title='Home', name='Andrew Pesotskiy')
 
 
 @app.route('/movies')
 def movies_page():
-    return render_template('movies.html', title='Movies list', movies=MOVIES)
+    return render_template(
+        'movies.html',
+        title='Movies list',
+        movies=MOVIES,
+        max_year=2010
+    )
 
 
-@app.route('/<title>')
-def movie_page(title):
+@app.route('/<id>')
+def movie_page(id):
+    """
+    I decided to use the ID in the link.
+    Because there are spaces in the title that do not look beautiful in the
+    link :)
+    """
     for i, movie in enumerate(MOVIES):
-        if MOVIES[i].get('title') == title:
-            return render_template('movie.html', title=title, movie=MOVIES[i])
+        if MOVIES[i].get('id') == id:
+            return render_template(
+                'movie.html',
+                title=MOVIES[i].get('title'),
+                movie=MOVIES[i]
+            )
     return render_template('movies.html', title='Movies list', movies=MOVIES)
 
 
