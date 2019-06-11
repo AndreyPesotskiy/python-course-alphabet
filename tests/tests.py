0a9b2b03d85007c3c117f6bf545f9896d9a2d10a
Andrey Pesotskiy <andrepesotskiy@gmail.com>
2019-06-11 10:51:15 +0300

    feat: each class has its own test

--- tests/tests.py
+++ tests/tests.py
@@ -27,27 +27,24 @@ def is_pickle(pickle_str):
     return True
 
 
-class TestSerialization(unittest.TestCase):
-    car_json_file = './data/json/car.json'
-    garage_pickle_file = './data/pickle/garage.txt'
-    cesar_yaml_file = './data/yaml/cesar.yaml'
+car = Car(
+    81.96,
+    'Sedan',
+    'BENTLEY',
+    24.3,
+)
+register_id = uuid.uuid4()
+garage = Garage(
+    'Kiev',
+    3,
+    register_id,
+    [car],
+)
+cesar = Cesar('cesar', [garage], register_id)
 
-    def __init__(self, *args, **kwargs):
-        super(TestSerialization, self).__init__(*args, **kwargs)
-        self.car = Car(
-            81.96,
-            'Sedan',
-            'BENTLEY',
-            24.3,
-        )
-        register_id = uuid.uuid4()
-        self.garage = Garage(
-            'Kiev',
-            3,
-            register_id,
-            [self.car],
-        )
-        self.cesar = Cesar('cesar', [self.garage], register_id)
+
+class TestCar(unittest.TestCase):
+    car_json_file = './data/json/car.json'
 
     def test_car_init_error(self):
         with self.assertRaises(IncorrectValueException):
@@ -60,7 +57,7 @@ class TestSerialization(unittest.TestCase):
             'BMW',
             52.1,
         )
-        self.assertEqual(self.car, car_other)
+        self.assertEqual(car, car_other)
 
     def test_car_ne(self):
         car_other = Car(
@@ -69,7 +66,7 @@ class TestSerialization(unittest.TestCase):
             'BMW',
             52.1,
         )
-        self.assertNotEqual(self.car, car_other)
+        self.assertNotEqual(car, car_other)
 
     def test_car_gt(self):
         car_other = Car(
@@ -78,7 +75,7 @@ class TestSerialization(unittest.TestCase):
             'BMW',
             52.1,
         )
-        self.assertGreater(self.car, car_other)
+        self.assertGreater(car, car_other)
 
     def test_car_ge(self):
         car_other = Car(
@@ -87,7 +84,7 @@ class TestSerialization(unittest.TestCase):
             'BMW',
             52.1,
         )
-        self.assertGreaterEqual(self.car, car_other)
+        self.assertGreaterEqual(car, car_other)
 
     def test_car_lt(self):
         car_other = Car(
@@ -96,7 +93,7 @@ class TestSerialization(unittest.TestCase):
             'BMW',
             52.1,
         )
-        self.assertLess(self.car, car_other)
+        self.assertLess(car, car_other)
 
     def test_car_le(self):
         car_other = Car(
@@ -105,16 +102,16 @@ class TestSerialization(unittest.TestCase):
             'BMW',
             52.1,
         )
-        self.assertLessEqual(self.car, car_other)
+        self.assertLessEqual(car, car_other)
 
     def test_car_change_number(self):
-        car_number = self.car.number
-        self.car.change_number()
-        car_number_2 = self.car.number
+        car_number = car.number
+        car.change_number()
+        car_number_2 = car.number
         self.assertNotEqual(str(car_number), str(car_number_2))
 
     def test_car_to_json_str(self):
-        self.assertTrue(is_json(self.car.to_json_str()))
+        self.assertTrue(is_json(car.to_json_str()))
 
     def test_car_instance_from_json_str(self):
         car_dict = {
@@ -125,19 +122,23 @@ class TestSerialization(unittest.TestCase):
             'number': str(uuid.uuid4())
         }
         self.assertIsInstance(
-            self.car.instance_from_json_str(json.dumps(car_dict)),
+            car.instance_from_json_str(json.dumps(car_dict)),
             Car
         )
 
     def test_car_to_json_file(self):
-        self.car.to_json_file(self.car_json_file)
+        car.to_json_file(self.car_json_file)
         self.assertTrue(isfile(self.car_json_file))
 
     def test_car_instance_from_json_file(self):
         self.assertIsInstance(
-            self.car.instance_from_json_file(self.car_json_file),
+            car.instance_from_json_file(self.car_json_file),
             Car
         )
+
+
+class TestGarage(unittest.TestCase):
+    garage_pickle_file = './data/pickle/garage.txt'
 
     def test_garage_init_error(self):
         with self.assertRaises(IncorrectValueException):
@@ -149,7 +150,7 @@ class TestSerialization(unittest.TestCase):
             1,
             uuid.uuid4()
         )
-        garage.add(self.car)
+        garage.add(car)
         self.assertEqual(len(garage.cars), 1)
 
     def test_garage_add_car_error(self):
@@ -157,7 +158,7 @@ class TestSerialization(unittest.TestCase):
             'Kiev',
             1,
             uuid.uuid4(),
-            [self.car],
+            [car],
         )
         car_other = Car(
             120.41,
@@ -173,19 +174,19 @@ class TestSerialization(unittest.TestCase):
             'Kiev',
             3,
             uuid.uuid4(),
-            [self.car]
+            [car]
         )
-        garage.remove(self.car)
+        garage.remove(car)
         self.assertEqual(len(garage.cars), 0)
 
     def test_garage_hit_hat(self):
-        self.assertEqual(self.garage.hit_hat(), 81.96)
+        self.assertEqual(garage.hit_hat(), 81.96)
 
     def test_garage_exist_car(self):
-        self.assertTrue(self.garage.exist_car(self.car))
+        self.assertTrue(garage.exist_car(car))
 
     def test_garage_to_pickle_str(self):
-        self.assertTrue(is_pickle(self.garage.to_pickle_str()))
+        self.assertTrue(is_pickle(garage.to_pickle_str()))
 
     def test_garage_instance_from_pickle_str(self):
         garage_dict = {
@@ -193,28 +194,32 @@ class TestSerialization(unittest.TestCase):
             'places': 3,
         }
         self.assertIsInstance(
-            self.garage.instance_from_pickle_str(pickle.dumps(garage_dict)),
+            garage.instance_from_pickle_str(pickle.dumps(garage_dict)),
             Garage
         )
 
     def test_garage_to_pickle_file(self):
-        self.garage.to_pickle_file(self.garage_pickle_file)
+        garage.to_pickle_file(self.garage_pickle_file)
         self.assertTrue(isfile(self.garage_pickle_file))
 
     def test_garage_instance_from_pickle_file(self):
         self.assertIsInstance(
-            self.garage.instance_from_pickle_file(self.garage_pickle_file),
+            garage.instance_from_pickle_file(self.garage_pickle_file),
             Garage
         )
 
+
+class TestCesar(unittest.TestCase):
+    cesar_yaml_file = './data/yaml/cesar.yaml'
+
     def test_cesar_hit_hat(self):
-        self.assertEqual(self.cesar.hit_hat(), 81.96)
+        self.assertEqual(cesar.hit_hat(), 81.96)
 
     def test_cesar_garages_count(self):
-        self.assertEqual(self.cesar.garages_count(), 1)
+        self.assertEqual(cesar.garages_count(), 1)
 
     def test_cesar_cars_count(self):
-        self.assertEqual(self.cesar.cars_count(), 1)
+        self.assertEqual(cesar.cars_count(), 1)
 
     def test_cesar_add_car(self):
         register_id = uuid.uuid4()
@@ -224,54 +229,54 @@ class TestSerialization(unittest.TestCase):
             register_id,
         )
         cesar = Cesar('Qwerty', [garage], register_id)
-        cesar.add_car(self.car, garage)
+        cesar.add_car(car, garage)
         self.assertEqual(cesar.cars_count(), 1)
 
     def test_cesar_eq(self):
         cesar_other = Cesar(
             'Griffin',
-            [self.garage]
+            [garage]
         )
-        self.assertEqual(self.cesar, cesar_other)
+        self.assertEqual(cesar, cesar_other)
 
     def test_cesar_ne(self):
         cesar_other = Cesar(
             'Griffin',
         )
-        self.assertNotEqual(self.cesar, cesar_other)
+        self.assertNotEqual(cesar, cesar_other)
 
     def test_cesar_gt(self):
         cesar_other = Cesar(
             'Griffin',
         )
-        self.assertGreater(self.cesar, cesar_other)
+        self.assertGreater(cesar, cesar_other)
 
     def test_cesar_ge(self):
         cesar_other = Cesar(
             'Griffin',
-            [self.garage]
+            [garage]
         )
-        self.assertGreaterEqual(self.cesar, cesar_other)
+        self.assertGreaterEqual(cesar, cesar_other)
 
     def test_cesar_lt(self):
         cesar_other = Cesar(
             'Griffin',
         )
-        self.assertLess(cesar_other, self.cesar)
+        self.assertLess(cesar_other, cesar)
 
     def test_cesar_le(self):
         cesar_other = Cesar(
             'Griffin',
-            [self.garage]
+            [garage]
         )
-        self.assertLessEqual(cesar_other, self.cesar)
+        self.assertLessEqual(cesar_other, cesar)
 
     def test_cesar_to_yaml_file(self):
-        self.cesar.to_yaml_file(self.cesar_yaml_file)
+        cesar.to_yaml_file(self.cesar_yaml_file)
         self.assertTrue(isfile(self.cesar_yaml_file))
 
     def test_cesar_instance_from_yaml_file(self):
         self.assertIsInstance(
-            self.cesar.instance_from_yaml_file(self.cesar_yaml_file),
+            cesar.instance_from_yaml_file(self.cesar_yaml_file),
             Cesar
         )
